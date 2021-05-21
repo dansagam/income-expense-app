ecc1a636bade25e23ee8037dfff9c21a596759e4
dansagam <48393847+dansagam@users.noreply.github.com>
2021-05-21 06:28:45 +0100

    adding the span tag to the h2 month  value

--- src/components/TopIncomeExpenseTop.js
+++ src/components/TopIncomeExpenseTop.js
@@ -5,7 +5,7 @@ const TopIncomeExpenseTop = () => {
     return (
         <div className="income-expense-details">
             <div className="header-text-div">
-                <h2 className="header-text">Welcome to your budget  of the %Month%</h2>
+                <h2 className="header-text">Welcome to your budget  of the <span>%Month%</span> </h2>
             </div>
             <div className="income-expense-display">+ 0</div>
             <ExpenseIncomeDisplay />
