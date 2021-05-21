7c084571b1bbe4eab777a3bda04f44fd8fbccddd
dansagam <48393847+dansagam@users.noreply.github.com>
2021-05-21 04:42:00 +0100

    add the label and amount to display

---
+++ src/components/IncomeDisplay.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const IncomeDisplay = () => {
+    return (
+        <div className="income-stack-display">
+            <div className="income-label">Income</div>
+            <div className="total-income-amount-field">
+                <div className="income-amount">0</div>
+                <div className="income-amount-percentage">&nbsp;</div>
+            </div>
+        </div>
+    )
+}
+
+export default IncomeDisplay
