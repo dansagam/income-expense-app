c08b72c5abf87433005cc8dd34f2fedf261b01ba
dansagam <48393847+dansagam@users.noreply.github.com>
2021-05-21 05:06:14 +0100

    breakdown the component

--- src/components/ExpenseIncomeDisplay.js
+++ src/components/ExpenseIncomeDisplay.js
@@ -4,14 +4,10 @@ import IncomeDisplay from './IncomeDisplay'
 
 const ExpenseIncomeDisplay = () => {
     return (
-        <div className="income-expense-details">
-            <div className="header-text-div">
-                <h2 className="header-text">Welcome to your budget  of the %Month%</h2>
-            </div>
-            <div className="income-expense-display">+ 0</div>
+        <>
             <IncomeDisplay />
             <ExpenseDisplay />
-        </div>
+        </>
     )
 }
 
