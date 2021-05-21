bebc7534f224b50280a592915d0d1a45942ffd8e
dansagam <48393847+dansagam@users.noreply.github.com>
2021-05-21 05:08:15 +0100

    add the income and expense to the component

---
+++ src/components/TopIncomeExpenseTop.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ExpenseIncomeDisplay from './ExpenseIncomeDisplay'
+
+const TopIncomeExpenseTop = () => {
+    return (
+        <div className="income-expense-details">
+            <div className="header-text-div">
+                <h2 className="header-text">Welcome to your budget  of the %Month%</h2>
+            </div>
+            <div className="income-expense-display">+ 0</div>
+            <ExpenseIncomeDisplay />
+        </div>
+    )
+}
+
+export default TopIncomeExpenseTop
