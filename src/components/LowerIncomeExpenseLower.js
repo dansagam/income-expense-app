63eda676302cbbaf7b1275163a2e54fb06c2003d
dansagam <48393847+dansagam@users.noreply.github.com>
2021-05-22 12:20:29 +0100

    added the lower field list

--- src/components/LowerIncomeExpenseLower.js
+++ src/components/LowerIncomeExpenseLower.js
@@ -7,14 +7,37 @@ const LowerIncomeExpenseLower = () => {
             <div className="input-income-expense-field">
                 <IncomeExpenseInputPanel />
             </div>
-            <div className="income-expense-list-field">
+            <div className="income-expense-list-container">
                 <div className="income-side-detail">
                     <h2 className="income-header">Income</h2>
-                    <div> list</div>
+                    <div className="income-list-container">
+                        <div className="item-field" id="div_0">
+                            <div className="item-description">descriiption</div>
+                            <div className="item-value-container">
+                                <div className="item-value">2342</div>
+                                <div className="delete-edit-button">
+                                    <button><span>edit</span></button>
+                                    <button className="remove-button"><span>delete</span></button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className="expense-side-detail">
                     <h2 className="expense-header">Expense</h2>
-                    <div>list</div>
+                    <div className="expense-list-container">
+                        <div className="item-field" id="div_0">
+                            <div className="item-description">descriiption</div>
+                            <div className="item-value-container">
+                                <div className="item-value">2342</div>
+                                <div className="delete-edit-button">
+                                    <button><span>edit</span></button>
+                                    <button className="remove-button"><span>delete</span></button>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
                 </div>
             </div>
             
