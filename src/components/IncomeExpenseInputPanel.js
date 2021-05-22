f5284624b96db104c441e98ffa8fbd3e2a48d29d
dansagam <48393847+dansagam@users.noreply.github.com>
2021-05-22 10:30:51 +0100

    remove the di tag

--- src/components/IncomeExpenseInputPanel.js
+++ src/components/IncomeExpenseInputPanel.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 const IncomeExpenseInputPanel = () => {
     return (
-        <div>
+        <>
             <select name="" id="">
                 <option value="income">Income</option>
                 <option value="expense">Expense</option>
             </select>
             <input type="text" />
             <input type="number" />
-        </div>
+        </>
     )
 }
 
