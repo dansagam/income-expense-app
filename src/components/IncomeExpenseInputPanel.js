cdb92d16e58f98c19e41d4e24afdcba4794adfa4
dansagam <48393847+dansagam@users.noreply.github.com>
2021-05-22 11:11:55 +0100

    add a  class input contatiner

--- src/components/IncomeExpenseInputPanel.js
+++ src/components/IncomeExpenseInputPanel.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 const IncomeExpenseInputPanel = () => {
     return (
-        <>
-            <select name="" id="">
+        <div className="input-container">
+            <select name="" id="income-expense-option">
                 <option value="income">Income</option>
                 <option value="expense">Expense</option>
             </select>
-            <input className="detail-input-text" type="text" />
-            <input type="number" className="input-detail-value" />
-        </>
+            <input className="detail-input-text" placeholder="Enter the details" type="text" />
+            <input placeholder="value" type="number" className="input-detail-value" />
+        </div>
     )
 }
 
