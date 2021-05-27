4e0ea6bec381cce6efbe097ee299b7338f6aa7d0
dansagam <48393847+dansagam@users.noreply.github.com>
2021-05-27 04:12:31 +0100

    define the global state and the reducer

---
+++ src/context/IEGlobalState.js
@@ -0,0 +1,54 @@
+import React, { createContext, useReducer } from 'react'
+import ExpenseReducer from './ExpenseReducer'
+import IncomeReducer from './IncomeReducer'
+
+
+const initialState ={
+    incomeTransactions: [
+        {id: 1, incomeDetail: 'book', incomeAmount: 5000},
+        {id: 2, incomeDetail: 'sex', incomeAmount: 6000}
+    ],
+    expenseTransactions : [
+        {id: 1, expenseDetail: 'porn', expenseAmount: 4000}
+    ]
+}
+
+export const GlobalContext = createContext(initialState)
+export const GlobalIncomeProvider = ({children}) => {
+    const [incomeState, dispatchIncome] = useReducer(IncomeReducer, initialState.incomeTransactions);
+    const [expenseState, dispatchExpense] = useReducer(ExpenseReducer, initialState.expenseTransactions);
+    const addIncomeTransaction = (incomeTransaction) => {
+        dispatchIncome({
+            type: 'ADD_INCOME_TRANSACTION',
+            payload: incomeTransaction
+        })
+    }
+    const deleteIncomeTransaction = (id) =>{
+        dispatchIncome({
+            type: 'DELETE_INCOME_TRANSACTION',
+            payload: id
+        })
+    }
+    const addExpenseTransaction = (expenseTransaction) => {
+        dispatchExpense({
+            type: 'ADD_EXPENSE_TRANSACTION',
+            payload: expenseTransaction
+        })
+    }
+    const deleteExpenseTransaction = (id) => {
+        dispatchExpense({
+            type: 'DELETE_EXPENSE_TRANSACTION',
+            payload: id
+        })
+    }
+
+    return <GlobalContext.Provider value={
+        {incomeTransactions: incomeState.incomeTransactions, 
+            expenseTransactions: expenseState.expenseTransactions,
+            deleteIncomeTransaction, addIncomeTransaction,
+            addExpenseTransaction, deleteExpenseTransaction
+        }
+    }>
+        {children}
+    </GlobalContext.Provider>
+}
