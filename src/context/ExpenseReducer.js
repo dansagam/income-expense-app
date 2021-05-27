5ca467a0408a9366dfbda734f51904bd2245bbbb
dansagam <48393847+dansagam@users.noreply.github.com>
2021-05-27 04:21:24 +0100

    adding the case switch for the action payload

---
+++ src/context/ExpenseReducer.js
@@ -0,0 +1,13 @@
+
+const ExpenseReducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD_EXPENSE_TRANSACTION':
+            return ;
+        case 'DELETE_EXPENSE_TRANSACTION':
+            return ;
+        default:
+            return;
+    }
+}
+
+export default ExpenseReducer
---
+++ src/context/IncomeReducer.js
@@ -0,0 +1,14 @@
+
+const IncomeReducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD_EXPENSE_TRANSACTION':
+            return ;
+        case 'DELETE_INCOME_TRANSACTION':
+            return
+    
+        default:
+            return;
+    }
+}
+
+export default IncomeReducer
