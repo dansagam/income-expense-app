0d87a3eb151f44bc7c4c4efc42b1d49042fddcd9
dansagam <48393847+dansagam@users.noreply.github.com>
2021-07-06 15:59:39 +0100

    finish up with the display on the balance

--- src/components/TopIncomeExpenseTop.js
+++ src/components/TopIncomeExpenseTop.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import {useContext} from 'react'
+import { GlobalContext } from '../context/IEGlobalState'
 import ExpenseIncomeDisplay from './ExpenseIncomeDisplay'
 
 const TopIncomeExpenseTop = () => {
+    const {expenseTransactions, incomeTransactions} = useContext(GlobalContext)
+    const iteratedAmount = incomeTransactions.map(incomeTransaction => incomeTransaction.amount)
+    const totalIncome = iteratedAmount.reduce((acc, item) => (acc +=item), 0).toFixed(2)
+    const totalExpenseAmount = expenseTransactions.map(expenseTransaction => expenseTransaction.amount)
+    const totalExpense = totalExpenseAmount.reduce((acc, item) => (acc += item), 0).toFixed(2)
+    let todayDate = new Date()
+    let options = {month: 'long'}
+    const todayMonth = new Intl.DateTimeFormat('en-US', options).format(todayDate);
+
     return (
         <div className="income-expense-details">
             <div className="header-text-div">
-                <h2 className="header-text">Welcome to your budget  of the <span>%Month%</span> </h2>
+                <h2 className="header-text">Welcome to your budget of the <span>{todayMonth}</span> </h2>
             </div>
-            <div className="income-expense-display">+ 0</div>
+            <div className="income-expense-display">{totalIncome - totalExpense}</div>
             <ExpenseIncomeDisplay />
         </div>
     )
