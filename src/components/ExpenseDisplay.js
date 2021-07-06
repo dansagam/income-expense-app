8d3974ce6e7ae6b82483e72175dbf622b8662026
dansagam <48393847+dansagam@users.noreply.github.com>
2021-07-06 15:58:14 +0100

    add the totalExpense and the percentag

--- src/components/ExpenseDisplay.js
+++ src/components/ExpenseDisplay.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import {useContext} from 'react'
+import { GlobalContext } from '../context/IEGlobalState'
 
 const ExpenseDisplay = () => {
+    const {expenseTransactions, incomeTransactions} = useContext(GlobalContext)
+    const iteratedAmount = incomeTransactions.map(incomeTransaction => incomeTransaction.amount)
+    const totalIncome = iteratedAmount.reduce((acc, item) => (acc +=item), 0).toFixed(2)
+    const totalExpenseAmount = expenseTransactions.map(expenseTransaction => expenseTransaction.amount)
+    const totalExpense = totalExpenseAmount.reduce((acc, item) => (acc += item), 0).toFixed(2)
+    const transactionPercetage = ((Number(totalExpense)/(Number(totalExpense) + Number(totalIncome))) *100).toFixed(2)
+        
+    // function valueCal(firstValue, nextVal){
+        
+    //     return firstValue + Number(nextVal) 
+    // }
     return (
         <div className="expense-stack-display">
             <div className="expense-label">Expense</div>
             <div className="total-expense-amount-field">
-                <div className="expense-amount">300</div>
-                <div className="expense-amount-percentage">40%</div>
+                <div className="expense-amount">{totalExpense}</div>
+                <div className="expense-amount-percentage">{transactionPercetage}<span>%</span></div>
             </div>
         </div>
     )
