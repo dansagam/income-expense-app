538f3b78997719d9c10ff62e14eeb531ddc5c753
dansagam <48393847+dansagam@users.noreply.github.com>
2021-07-06 15:58:54 +0100

    added the total income

--- src/components/IncomeDisplay.js
+++ src/components/IncomeDisplay.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { GlobalContext } from '../context/IEGlobalState'
 
 const IncomeDisplay = () => {
+    const {incomeTransactions} = useContext(GlobalContext)
+    const iteratedAmount = incomeTransactions.map(incomeTransaction => incomeTransaction.amount)
+    const totalIncome = iteratedAmount.reduce((acc, item) => (acc +=item), 0).toFixed(2)
+    // function valueCal(firstValue, nextVal){
+    //     return firstValue.amount + nextVal.amount
+    // }
+
     return (
         <div className="income-stack-display">
             <div className="income-label">Income</div>
             <div className="total-income-amount-field">
-                <div className="income-amount">0</div>
+                <div className="income-amount">{totalIncome}</div>
                 <div className="income-amount-percentage">&nbsp;</div>
             </div>
         </div>
