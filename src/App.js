7d5df7ce504fa1bebe7de73815673a919898a636
dansagam <olukayodeogunnowo@yahoo.com>
2021-08-04 11:41:37 +0100

    remove the globalContextProvider of the react Hook

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-
 import './App.css';
 import LowerIncomeExpenseLower from './components/LowerIncomeExpenseLower';
 import TopIncomeExpenseTop from './components/TopIncomeExpenseTop';
-import { GlobalIncomeProvider } from './context/IEGlobalState';
 
 function App() {
   return (
     <div className="App">
-        <GlobalIncomeProvider >
-            <div className="display-topper">
-                <TopIncomeExpenseTop />
-            </div>
-            <LowerIncomeExpenseLower />
-
-        </GlobalIncomeProvider>
+        <div className="display-topper">
+            <TopIncomeExpenseTop />
+        </div>
+        <LowerIncomeExpenseLower />
     </div>
   );
 }
