2b689452ec6958d3ec1c11a11c158649ea3855da
dansagam <olukayodeogunnowo@yahoo.com>
2021-08-14 21:18:44 +0100

    setting up the backend git repo

---
+++ models/Expenses.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+
+const ExpenseSchema = new mongoose.Schema({
+   description: {
+      type: String,
+      require: true
+   },
+   amount : {
+      type: Number,
+      require: true
+   },
+   timestamps: true
+})
+
+const Expense = mongoose.model('Expense', ExpenseSchema)
+
+export default Expense
---
+++ models/Incomes.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+
+const IncomeSchema = new mongoose.Schema({
+   description: {
+      type: String,
+      require: true,
+   },
+   amount: {
+      type: Number,
+      require: true
+   },
+   timestamps: true
+})
+
+const Income = mongoose.model('Income', IncomeSchema )
+
+export default Income
