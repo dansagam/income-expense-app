5aee95926d8250bf14bd4f2c3e35a8188136dd87
dansagam <olukayodeogunnowo@yahoo.com>
2021-08-15 23:17:04 +0100

    added the useEffect the get expense transaction

--- clients/src/App.js
+++ clients/src/App.js
@@ -3,12 +3,14 @@ import { useDispatch } from 'react-redux';
 import './App.css';
 import LowerIncomeExpenseLower from './components/LowerIncomeExpenseLower';
 import TopIncomeExpenseTop from './components/TopIncomeExpenseTop';
+import { getExpenseTransactions } from './context/ExpenseReducer';
 import { getIncomeTransactions } from './context/IncomeReducer';
 
 function App() {
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(getIncomeTransactions())
+    dispatch(getExpenseTransactions())
   },[dispatch])
   return (
     <div className="App">
