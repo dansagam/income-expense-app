16b6e89157936bbad62aee509305a851ca61a2c9
dansagam <olukayodeogunnowo@yahoo.com>
2021-08-15 19:02:03 +0100

    clear the excess comment line

--- clients/src/context/IncomeReducer.js
+++ clients/src/context/IncomeReducer.js
@@ -49,39 +49,11 @@ export const editIncomeTransaction = createAsyncThunk('Income/editIncomeTransact
 export const IncomeReducer = createSlice({
     name: 'Income',
     initialState: {
-        incomeTransactions:[
-            // {id: 1, description: 'book', amount: 5000},
-            // {id: 2, description: 'Music', amount: 6000}
-        ],
+        incomeTransactions:[],
         status: false,
         loading: false,
     },
     reducers: {
-        // addIncomeTransaction: (state, action) => {
-        //     return {
-        //         ...state,
-        //         incomeTransactions: [action.payload, ...state.incomeTransactions]
-        //     }
-        // },
-        // deleteIncometransaction: (state, action)=>{
-        //     return {
-        //         ...state,
-        //         incomeTransactions: state.incomeTransactions.filter(incomeTransaction => incomeTransaction.id !== action.payload)
-        //     }
-        // },
-        // editIncomeTransaction: (state, action) => {
-        //     const { id, description, amount} = action.payload
-        //     const existingIncome = state.incomeTransactions.find((incomeTransaction) => incomeTransaction.id === id)
-        //     if(existingIncome) {
-        //        existingIncome.description = description
-        //        existingIncome.amount = amount
-        //     }
-        // },
-        // return {
-        //     ...state,
-        //     incomeTransactions: [...state.incomeTransactions],
-        //     status: false
-        // }
         incomeEditStatus: (state, action)=>{
             return {
                 ...state,
@@ -128,39 +100,4 @@ export const { /**addIncomeTransaction, deleteIncometransaction, editIncomeTrans
 export const incomeSelectState = state => state.Income.incomeTransactions
 
 export default IncomeReducer.reducer
-
-// const IncomeReducer = (state, action) => {
-//     switch (action.type) {
-//         case 'ADD_INCOME_TRANSACTION':
-//             return {
-//                 ...state,
-//                 incomeTransactions: [action.payload, ...state.incomeTransactions]
-//             }
-//         case 'DELETE_INCOME_TRANSACTION':
-//             return {
-//                 ...state,
-//                 incomeTransactions: state.incomeTransactions.filter(incomeTransaction => incomeTransaction.id !== action.payload)
-//             }
-//         case 'EDIT_INCOME_TRANSACTION':
-//             const { id, description, amount} = action.payload
-//             const existingIncome = state.incomeTransactions.find((incomeTransaction) => incomeTransaction.id === id)
-//             if(existingIncome) {
-//                existingIncome.text = description
-//                existingIncome.amount = amount
-//             }
-//             return {
-//                 ...state,
-//                 incomeTransactions: [...state.incomeTransactions],
-//                 status: false
-//             }
-        
-//         case 'INCOME_EDIT_STATUS':
-//             return {
-//                 ...state,
-//                 status: action.payload
-//             }
-//         default:
-//             return state
-//     }
-// }
 
