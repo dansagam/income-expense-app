b008d3eef499355d49302fc1a2bc40a9bb6c85b0
dansagam <olukayodeogunnowo@yahoo.com>
2021-08-15 18:58:58 +0100

    create the useEffect to the get the seeded income transactions

--- clients/src/App.js
+++ clients/src/App.js
@@ -1,8 +1,15 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import './App.css';
 import LowerIncomeExpenseLower from './components/LowerIncomeExpenseLower';
 import TopIncomeExpenseTop from './components/TopIncomeExpenseTop';
+import { getIncomeTransactions } from './context/IncomeReducer';
 
 function App() {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(getIncomeTransactions())
+  },[dispatch])
   return (
     <div className="App">
         <div className="display-topper">
