2399aa1be8e1f49805feba44fcf993147252d72c
dansagam <olukayodeogunnowo@yahoo.com>
2021-08-16 11:55:12 +0100

    change the port listening name

--- server.js
+++ server.js
@@ -31,6 +31,5 @@ const PORT = process.env.PORT || 5000
 
 app.listen(
     PORT, () => 
-    console.log(`MERN shopping app 
-        ${process.env.NODE_ENV} listening on port ${PORT} 🔥 !` )
+    console.log(`Budget income app  ${process.env.NODE_ENV} listening on port ${PORT} 🔥 !` )
 )
