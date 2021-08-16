3255d187a2f52d3d4db798674cac252a8841a29a
dansagam <olukayodeogunnowo@yahoo.com>
2021-08-16 15:09:22 +0100

    set up the user various controllers

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -9,32 +9,40 @@ import generateToken from '../utils/generateToken.js'
     @access public
 */
 const registerUser = async (req, res, next) =>{
-   const {name, email, password} = req.body
-
-   const existingUser = await User.findOne({email})
-
-   if(existingUser){
-      res.status(400)
-      throw new Error('User already exist')
-   }
-   const user = await User.create({
-      name, 
-      email, 
-      password
-   })
-
-   if(user) {
-      res.status(201).json({
-         _id: user._id,
-         name: user.name,
-         email: user.email,
-         isAdmin: user.isAdmin,
-         token: generateToken(user._id),
-
+   try {
+      const {name, email, password} = req.body
+   
+      const existingUser = await User.findOne({email})
+   
+      if(existingUser){
+         res.status(400)
+         throw new Error('User already exist')
+      }
+      const user = await User.create({
+         name, 
+         email, 
+         password
       })
-   }else{
-      res.status(400)
-      throw new Error('Invalid user data')
+   
+      if(user) {
+         res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),
+   
+         })
+      }
+      else{
+         res.status(400)
+         throw new Error('Invalid user data')
+      }
+      
+   } catch (error) {
+      res.json({
+         msg: error.message
+      })
    }
 
 }
@@ -47,20 +55,27 @@ const registerUser = async (req, res, next) =>{
 
 
 const authUserLogin = async ( req, res, next) =>{
-   const {email, password} = req.body
-   const user = await User.findOne({email})
-   if (user && (await user.matchPasswords(password))) {
+   try {
+      const {email, password} = req.body
+      const user = await User.findOne({email})
+      if (user && (await user.matchPassword(password))) {
+         res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id)
+         })
+      }else {
+         res.status(401)
+         throw new Error('Invalid email or password')
+       }
+      
+   } catch (error) {
       res.json({
-         _id: user._id,
-         name: user.name,
-         password: user.password,
-         email: user.email,
-         token: generateToken(user._id)
+         msg: error.message
       })
-   }else {
-      res.status(401)
-      throw new Error('Invalid email or password')
-    }
+      
+   }
 }
 
 /*
@@ -70,8 +85,64 @@ const authUserLogin = async ( req, res, next) =>{
 */
 
 const getUsers = async (req, res, next) => {
-   const user = await User.find({})
-   res.json(user)
+   try {
+      const user = await User.find({})
+      res.json(user)
+      
+   } catch (error) {
+      res.status(404).json({
+         msg: 'User not found'
+      })
+      
+   }
 }
 
-export { registerUser, getUsers, authUserLogin}
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserProfile = async (req, res, next) =>{
+
+   try {
+      const userProfile = await User.findById(req.user._id)
+      res.json({
+         _id: userProfile._id,
+         name: userProfile.name,
+         email: userProfile.email
+      })
+      
+   } catch (err) {
+      res.status(404).json({
+         msg: 'User not found'
+      })
+   }
+}
+
+const updateUserProfile = async (req, res, next) =>{
+   try {
+      const user = await User.findById(req.user._id)
+      user.name = req.body.name || user.name
+      user.email = req.body.email || user.email
+      if (req.body.password) {
+         user.password = req.body.password
+      }
+      const updatedUser = await user.save()
+
+      res.json({
+         _id: updatedUser._id,
+         name: updatedUser.name,
+         email: updatedUser.email,
+         token: generateToken(updatedUser._id),
+      })
+   } catch (error) {
+      res.status(404)
+      throw new Error('User not found')
+   }
+}
+
+export { 
+   registerUser, 
+   getUsers, 
+   authUserLogin, 
+   getUserProfile,
+   updateUserProfile
+}
