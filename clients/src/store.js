c9f53ae7c35c126e414e1fb1d4eb037d5c333fea
dansagam <olukayodeogunnowo@yahoo.com>
2021-08-26 23:18:38 +0100

    added the actioncreator of use

--- clients/src/store.js
+++ clients/src/store.js
@@ -1,10 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit'
 import ExpenseReducer from './context/ExpenseReducer'
 import IncomeReducer from './context/IncomeReducer'
+import UserReducer from './context/userReducer'
 
 export default configureStore({
    reducer: {
       Expense: ExpenseReducer,
       Income: IncomeReducer,
+      User: UserReducer
    },
 })
