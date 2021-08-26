2f4321c13b5ce8e44b44cb57cf70d7e826f19047
dansagam <olukayodeogunnowo@yahoo.com>
2021-08-26 18:56:38 +0100

    modified the error handler  by using the next params

--- middlewares/authMiddlewares.js
+++ middlewares/authMiddlewares.js
@@ -6,40 +6,31 @@ config({path: '../config/config.env'})
 
 
 const authService = async (req, res, next) =>{
-   const token = req.header('x-auth-token')
-   if(!token){
-      res.status(401).json({msg: "unauthorised token"})
-   }
    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET)
-      req.user = await User.findById(decoded.id).select('-password')
-      next()
-   } catch (error) {
-      // res.status(400).json({msg: 'token not valid'})
-      res.json({
-         msg: error.message
-      })
-      // console.error(error)
-      // res.status(401)
-      // throw new Error('Not authorized, token failed')
+      let token
+      if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+         token = req.headers.authorization.split(' ')[1]
+         try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+            if (decoded){
+               req.user = await User.findById(decoded.id).select('-password')
+            }
+         } catch (err) {
+            res.status(401)
+            throw new Error('Not authorized, token verification failed')
+            
+         }
+         next()
+      }else {
+         res.status(401)
+         throw new Error('Not authorized, token failed') 
+      }
+      
+   } catch (err) {
+      // res.status(400)
+      // throw new Error('Not authorized, bad request token failed') 
+      next(err)
    }
-
-   // let token
-   // if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
-   //    try {
-   //       token = req.headers.authorization.split(' ')[1]
-   //       const decoded = jwt.verify(token, process.env.JWT_SECRET)
-   //       req.user = await User.findById(decoded.id).select('-password')
-   //       next()
-         
-   //    } catch (err) {
-   //       console.error(error)
-   //       res.status(401)
-   //       throw new Error('Not authorized, token failed')
-         
-   //    }
-
-   // }
 }
 
 export  { authService }
