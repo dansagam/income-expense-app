16ed407c0334bd712c4eaaf89de4351c59d36f0d
dansagam <olukayodeogunnowo@yahoo.com>
2021-08-30 16:00:33 +0100

    handled the empty token

--- middlewares/authMiddlewares.js
+++ middlewares/authMiddlewares.js
@@ -6,21 +6,26 @@ config({path: '../config/config.env'})
 
 
 const authService = async (req, res, next) =>{
+   console.log(req.headers.authorization)
    try {
       let token
       if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
          token = req.headers.authorization.split(' ')[1]
-         try {
-            const decoded = jwt.verify(token, process.env.JWT_SECRET)
-            if (decoded){
-               req.user = await User.findById(decoded.id).select('-password')
+         if(token){
+            try {
+               const decoded = jwt.verify(token, process.env.JWT_SECRET)
+               if (decoded){
+                  req.user = await User.findById(decoded.id).select('-password')
+                  next()
+               }
+            } catch (err) {
+               res.status(401)
+               throw new Error('Not authorized, token verification failed')  
             }
-         } catch (err) {
+         }else {
             res.status(401)
-            throw new Error('Not authorized, token verification failed')
-            
+            throw new Error('Not authorized, token cannot be empty token failed')
          }
-         next()
       }else {
          res.status(401)
          throw new Error('Not authorized, token failed') 
